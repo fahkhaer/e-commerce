e2a9a3d0fab4f4a2c51fd5dcaebd1a6679329c42
fahkhaer <khaeranifah@gmail.com>
2025-10-04 15:11:18 +0700

    set provider

--- src/providers/AuthProvider.tsx
+++ src/providers/AuthProvider.tsx
@@ -1,1 +1,40 @@
-//isinya ada di-ss
+'use client'
+import { createContext, useEffect, useState } from "react";
+
+interface User {
+  token: string;
+  name?: string;
+}
+
+interface AuthContextType {
+  user: User | null;
+  login: (user: User) => void;
+  logout: () => void;
+}
+
+export const AuthContext = createContext<AuthContextType | null>(null);
+
+export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const saved = localStorage.getItem("user");
+    if (saved) setUser(JSON.parse(saved));
+  }, []);
+
+  const login = (newUser: User) => {
+    localStorage.setItem("user", JSON.stringify(newUser));
+    setUser(newUser);
+  };
+
+  const logout = () => {
+    localStorage.removeItem("user");
+    setUser(null);
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
