4974413b7853f5a6d33099939cec8339c5bcf621
fahkhaer <khaeranifah@gmail.com>
2025-10-04 15:10:30 +0700

    api config

--- src/lib/api-clients.ts
+++ src/lib/api-clients.ts
@@ -1,1 +1,13 @@
 //isinya ada di-ss
+import axios from 'axios';
+import { APIConfiguration } from '@/config/api.config';
+
+const url = APIConfiguration.baseUrl || "https://e-commerce-api-production-26ab.up.railway.app"
+
+export const apiClient = axios.create({
+  baseURL: `${url}/api`,
+  timeout: 8000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
