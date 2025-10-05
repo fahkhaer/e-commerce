f638f47fe918e7cd8ed7da12516f9a1e096c1de5
fahkhaer <khaeranifah@gmail.com>
2025-10-05 16:45:43 +0700

    detail page

--- src/components/layouts/Catalog.tsx
+++ src/components/layouts/Catalog.tsx
@@ -9,8 +9,11 @@ import { Button } from "../ui/button";
 import { cn } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function Catalog() {
+  const router = useRouter();
+
   const [visibleItems, setVisibleItems] = useState(16);
 
   const { data, isLoading } = useQuery<iProduct[]>({
@@ -33,9 +36,9 @@ export default function Catalog() {
           <div className="grid gap-5 grid-cols-2 md:grid-cols-4">
             {/* put card product here */}
             {dataToDisplay?.map((item: iProduct) => (
-              <div key={item.id} className="shadow-[0_0_20px_0_#CBCACA40]">
+              <div key={item.id} className="shadow-[0_0_20px_0_#CBCACA40]" 
+              onClick={() => router.push(`/detail-product/${item.id}`)} >
                 {/* product image */}
-                <Link href="/detail-product">
                   <Image
                     className="h-auto w-full"
                     src={item.images[0] || "productexample.png"}
@@ -43,7 +46,6 @@ export default function Catalog() {
                     width={100}
                     height={500}
                   />
-                </Link>
                 {/* product description */}
                 <div className="p-3 ">
                   <div className="text-sm md:text-base hover:text-primary hover:scale-105 transition duration-500 block">
