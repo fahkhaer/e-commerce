9fcf1c5fe5d543ff63b76f32a49295567efeb958
fahkhaer <khaeranifah@gmail.com>
2025-10-10 14:50:58 +0700

    fix: allow all routes

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: [
-      "res.cloudinary.com",
-      "images.macrumors.com",
-      "artofcards.in",
-      "images.unsplash.com",
-      "www.mobiledokan.com",
-      "e-commerce-theta-liart-83.vercel.app"
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*',
+      },
+      {
+        protocol: 'http',
+        hostname: '*',
+      },
     ],
   },
   reactStrictMode: true,
